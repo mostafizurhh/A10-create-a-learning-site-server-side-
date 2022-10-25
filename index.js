a2fed75e338ff3138a1184d01ec0a0391c4f1ee4
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-25 13:33:52 +0100

    installed cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
 const courses = require('./data/courses.json')
-const courseDetail = require('./data/courseDetail.json')
 
 app.get('/', (req, res) => {
     res.send('loading data from server')
@@ -12,11 +14,11 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
-// app.get('/courses/:id', (req, res) => {
-//     const id = req.params.id;
-//     const individualCourse = courses.find(course => course.id === id);
-//     res.send(individualCourse);
-// })
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const individualCourse = courses.find(course => course.id === id);
+    res.send(individualCourse);
+})
 
 app.listen(port, () => {
     console.log(`listening from port ${port}`)
